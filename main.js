5f2b8040fac9ae0785fcadb8d2dd02627aa6ffdc
Eddy Mead <wilddamon@gmail.com>
2017-04-21 00:41:24 +1000

    Added another card for schedule, made two icons on a map

--- main.js
+++ main.js
@@ -12,3 +12,28 @@ function init() {
     selected = target.css('z-index', 200);
   });
 }
+
+
+function initMap() {
+  var island = {lat: -33.863539, lng: 151.1531};
+  var wharf = {lat: -33.869279, lng: 151.151874};
+  var center = {
+    lat: (island.lat * 2 + wharf.lat)/3,
+    lng: (island.lng + wharf.lng)/2,
+  }
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: center
+  });
+  new google.maps.Marker({
+    position: island,
+    map: map,
+    icon: 'photos/roses.svg',
+  });
+  new google.maps.Marker({
+    position: wharf,
+    map: map,
+    icon: 'photos/ferry_icon.svg',
+  });
+}
